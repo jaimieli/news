d920b1d467e59f60af75fe9f2c9c94577638c1f1
jamcodes <jaimieli@gmail.com>
2014-09-20 16:33:23 -0400

    trends displaying in template

--- server/api/gNew/gNew.controller.js
+++ server/api/gNew/gNew.controller.js
@@ -5,7 +5,7 @@ var Gnew = require('./gNew.model');
 var request = require('request');
 var async = require('async');
 var AlchemyAPI = require('alchemy-api');
-var alchemy = new AlchemyAPI('444606205f63cf2a1943e4071ba69cc6500d7b7d');
+var alchemy = new AlchemyAPI('daca9d2d07da11c0a9e7bd1cba99e590b8e6b387');
 var wikipedia = require('wikipedia-js');
 var _ = require('underscore');
 
